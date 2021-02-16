efd570a772847a8f86ab0a04fccbe0f1a1a9e48d
Artyom <nikulin_artem@mail.ru>
2021-02-16 23:55:45 +0500

    Перенос

--- App.js
+++ App.js
@@ -1,7 +1,8 @@
 import React, {useEffect, useState} from 'react';
 import MapView, {Marker} from 'react-native-maps';
-import { StyleSheet, Text, View, Dimensions, ActivityIndicator } from 'react-native';
-//import Geolocation from 'react-native-geolocation-service'; 
+import { StyleSheet, Text, View, Dimensions, ActivityIndicator, Image, TextInput } from 'react-native';
+import { Dialog } from 'react-native-ui-lib'
+
 
 const initialState = {
     latitude: null,
@@ -12,6 +13,62 @@ const initialState = {
 
 export default function App() {
   const [currentPosition, setCurrentPosition] = useState(initialState)
+  let [markers, setMarkers] = useState([
+    {
+      id: 0,
+      region: { latitude: 58.1373603879051, longitude: 56.31226414142721 },
+      title: 'EKA HOSPITAL',
+      desc: 'Telp.(021)8888800',
+      urlImg: 'https://serpongku.com/wp-content/uploads/2018/08/Eka-Hospital-BSD-City.jpg'
+  }
+])
+  const [marker, setMarker] = useState({})
+  //const [detailName, setDetailName] = useState(marker['title'])
+
+  function openModalMarker() {
+    return  <Dialog
+      height={1} center
+  ></Dialog>
+  }
+  
+  function addMarkerOnMap(e) {
+    const newId = markers[markers.length - 1].id + 1
+    const title = 'Test Marker'
+    const desc = 'Test description'
+    setMarkers([
+      ...markers,
+      {
+        id: newId,
+        region: {latitude: e.nativeEvent.coordinate.latitude, longitude: e.nativeEvent.coordinate.longitude},
+        title: title,
+        desc: desc,
+        urlImg: 'https://serpongku.com/wp-content/uploads/2018/08/Eka-Hospital-BSD-City.jpg'
+      }
+    ])
+  }
+
+  const renderMarker = () => {
+    console.log('Зашел в рендер')
+    console.log(markers)
+    return markers.map(_marker => (
+      <Marker
+        key={_marker['id']}
+        coordinate={_marker['region']}
+        title={_marker['title']}
+        description={_marker['desc']}
+        onPress={() => {
+          setMarker(_marker);
+          openModalMarker()
+        }}
+      />
+    ));
+  }
+
+  const renderDetailMarker = () => (
+    <Dialog
+        height={1} padding-card marginB-s4
+      ></Dialog>
+  )
 
   useEffect(() => {
     navigator.geolocation.getCurrentPosition(position => {
@@ -28,7 +85,6 @@ export default function App() {
   }, [])
     
   function onRegionChange(region) {
-    console.log(region)
     setCurrentPosition(region)
   }
   
@@ -39,7 +95,11 @@ export default function App() {
         initialRegion={currentPosition} 
         onRegionChange={onRegionChange} 
         showsUserLocation={true}
-      />
+        onPress={addMarkerOnMap}
+      >
+        {renderMarker()}
+      </MapView>
+      { marker.hasOwnProperty('id') && renderDetailMarker() }
     </View>
   ) : <ActivityIndicator style={{flex: 1}} animating size="large" />
 }
